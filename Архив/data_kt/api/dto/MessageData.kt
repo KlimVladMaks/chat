8f632bd2c2417720046778b515b65f26ff26e691
vlad <vkm.blog@yandex.ru>
2026-05-10 23:15:22 +0300

    Commit 4

--- Архив/data_kt/api/dto/MessageData.kt
+++
@@ -1,6 +0,0 @@
-package io.github.klimvladmaks.chat.data.api.dto
-
-sealed class MessageData {
-    data class TextMessage(val text: String) : MessageData()
-    data class ImageMessage(val link: String? = null) : MessageData()
-}
--- Архив/data_kt/repository/UnauthorizedException.kt
+++
@@ -1,8 +0,0 @@
-package io.github.klimvladmaks.chat.data.repository
-
-
-/**
- * Исключение, выбрасываемое репозиторием при отсутствии токена
- * или получении HTTP 401 от сервера.
- */
-class UnauthorizedException : Exception("Authentication token is missing or expired")
